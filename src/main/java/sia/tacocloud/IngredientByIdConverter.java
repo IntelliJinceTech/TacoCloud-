fb2ddb0b14f158f93cc59d75317f91d46c6def5f
Christopher Jin <chrisjin1@gmail.com>
2022-09-03 22:35:04 -0400

    start validation

--- src/main/java/sia/tacocloud/IngredientByIdConverter.java
+++ src/main/java/sia/tacocloud/IngredientByIdConverter.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import tacos.Ingredient;
-import tacos.Ingredient.Type;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
@@ -16,25 +14,25 @@ public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
     public IngredientByIdConverter() {
         ingredientMap.put("FLTO",
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
         ingredientMap.put("COTO",
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
         ingredientMap.put("GRBF",
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
         ingredientMap.put("CARN",
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
         ingredientMap.put("TMTO",
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
         ingredientMap.put("LETC",
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
         ingredientMap.put("CHED",
-                new Ingredient("CHED", "Cheddar", Type.CHEESE));
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
         ingredientMap.put("JACK",
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
         ingredientMap.put("SLSA",
-                new Ingredient("SLSA", "Salsa", Type.SAUCE));
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
         ingredientMap.put("SRCR",
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
     }
 
     @Override
--- src/main/java/sia/tacocloud/OrderController.java
+++ src/main/java/sia/tacocloud/OrderController.java
@@ -3,10 +3,8 @@ package sia.tacocloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -17,6 +15,15 @@ public class OrderController {
     @GetMapping("/current") //combined with RequestMapping, this means that the orderForm() method will handle HTTP GET requests for /orders/current
     public String showOrderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete(); //TacoOrder was initially created and placed into the session when the user created their first taco. by
+        // calling setComplete(), we are ensuring that the session is cleaned up and ready for a new order the next time the user creates a taco
+
+        return "redirect:/";
     }
 }
 
